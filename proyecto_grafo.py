be8eead01c34332df9c58039fa0233199dd1c309
Incorregibletar23 <lalosamaaan@outlook.com>
2025-05-31 15:34:57 -0600

    Se empezó a hacer la interfaz grafica en grafos

--- proyecto_grafo.py
+++ proyecto_grafo.py
@@ -1,16 +1,31 @@
+# 1.- ---------- Encabezado ----------
+
+'''
+Programa: Proyecto Grafo - proyecto_grafo.py
+Autor: Ramírez Vásquez Eduardo
+Fecha de creación: 14/05/2025
+Fechas de modificación:
+- 31/05/2025 2:30 pm(v2.1: Se empezará a hacer la interfaz gráfica del proyecto)
+'''
+
+# 2.- ---------- Importación de módulos y bibliotecas ----------
+
+#           IMPORTACIONES PARA TERMINAL
+'''
 import os
-from IPython.display import clear_output
 import networkx as nx
 import matplotlib.pyplot as plt
 import tracemalloc
 import time
+'''
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
 
+# 3.- ---------- Definición de funciones o clases ----------
 
-
-
-
-
-
+#           CLASES PARA USO EN LA TERMINAL
+'''
 class NodoDoble:
     def __init__(self, destino, peso):
         self.destino = destino
@@ -21,7 +36,6 @@ class NodoDoble:
 class ListaDobleEnlazada:
     def __init__(self):
         self.inicio = None
-
     def insertarFinal(self, destino, peso):
         nuevo_nodo = NodoDoble(destino, peso)
         if not self.inicio:
@@ -32,7 +46,6 @@ class ListaDobleEnlazada:
             actual = actual.siguiente
         actual.siguiente = nuevo_nodo
         nuevo_nodo.anterior = actual
-
     def eliminar(self, destino):
         actual = self.inicio
         while actual:
@@ -46,14 +59,12 @@ class ListaDobleEnlazada:
                 return True
             actual = actual.siguiente
         return False
-
     def mostrar(self):
         actual = self.inicio
         while actual:
             print(f"{actual.destino}--{actual.peso}--", end=" -> ")
             actual = actual.siguiente
         print("None")
-
     def lista_de_aristas(self):
         aristas = []
         actual = self.inicio
@@ -65,7 +76,6 @@ class ListaDobleEnlazada:
 class Grafo:
     def __init__(self):
         self.grafo = {}
-
     def agregar_vertice(self):
         while True:
             try:
@@ -82,7 +92,6 @@ class Grafo:
                 self.grafo[vertice] = ListaDobleEnlazada()
                 print(f"Sala '{vertice}' agregada.")
                 time.sleep(2.2)
-
     def agregar_arista(self):
         while True:
             try:
@@ -110,7 +119,6 @@ class Grafo:
             else:
                 print(f'{nodo_1} no existe.')
                 time.sleep(2.2)
-
     def eliminar_arista(self):
         origen = input('Ingresa sala de origen: ')
         destino = input('Ingresa sala de destino: ')
@@ -134,7 +142,6 @@ class Grafo:
         except (KeyError, ValueError) as e:
             print(e)
             time.sleep(2.5)
-
     def eliminar_vertice(self):
         vertice = input('Ingresa la sala que quieres eliminar: ')
         try:
@@ -150,7 +157,6 @@ class Grafo:
         except KeyError as e:
             print(e)
             time.sleep(2.2)
-
     def encontrar(self):
         vertice = input('Ingresa sala a buscar: ')
         if vertice in self.grafo:
@@ -160,20 +166,17 @@ class Grafo:
         else:
             print(f"La sala: '{vertice}' no existe.")
             time.sleep(2.2)
-
     def ver_grafo(self):
         print("Mostrando salas en lista:")
         for vertice, lista in self.grafo.items():
             print(f"{vertice} -> ", end="")
             lista.mostrar()
         time.sleep(7)
-
     def matriz_de_adyacencia(self):
         vertices = list(self.grafo.keys())
         n = len(vertices)
         indices = {nodo: i for i, nodo in enumerate(vertices)}
         matriz = [[0] * n for _ in range(n)]
-
         for vertice, lista in self.grafo.items():
             origen_idx = indices[vertice]
             aristas = lista.lista_de_aristas()
@@ -184,11 +187,20 @@ class Grafo:
         for fila in matriz:
             print(fila)
         return matriz
-
+    def visualizar(self):
+        G = nx.DiGraph()
+        for origen, lista in self.grafo.items():
+            for destino, peso in lista.lista_de_aristas():
+                G.add_edge(origen, destino, weight=peso)
+        pos = nx.spring_layout(G)
+        nx.draw(G, pos, with_labels=True, node_color='skyblue', node_size=2000, arrows=True)
+        edge_labels = nx.get_edge_attributes(G, 'weight')
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+        plt.title("Mapa de salas")
+        plt.show()
     def menu(self):
         print()
         while True:
-            clear_output()
             print("--- Bienvenido administrador ---")
             print("1. Agregar salas")
             print("2. Agregar caminos")
@@ -205,10 +217,8 @@ class Grafo:
                 opcion = input("Selecciona una opción: ")
                 if opcion == '1':
                     self.agregar_vertice()
-                    clear_output()
                 elif opcion == '2':
                     self.agregar_arista()
-                    clear_output()
                 elif opcion == '3':
                     self.ver_grafo()
                 elif opcion == '4':
@@ -216,10 +226,8 @@ class Grafo:
                     time.sleep(10)
                 elif opcion == '5':
                     self.eliminar_vertice()
-                    clear_output()
                 elif opcion == '6':
                     self.eliminar_arista()
-                    clear_output()
                 elif opcion == '7':
                     self.encontrar()
                 elif opcion == '8':
@@ -235,28 +243,12 @@ class Grafo:
             except ValueError as e:
                 print(e)
 
-    def visualizar(self):
-        G = nx.DiGraph()
-        for origen, lista in self.grafo.items():
-            for destino, peso in lista.lista_de_aristas():
-                G.add_edge(origen, destino, weight=peso)
-
-        pos = nx.spring_layout(G)
-        nx.draw(G, pos, with_labels=True, node_color='skyblue', node_size=2000, arrows=True)
-        edge_labels = nx.get_edge_attributes(G, 'weight')
-        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-        plt.title("Mapa de salas")
-        plt.show()
-
-
-
 class BusquedaDeCamino:
     def __init__(self, matriz, origen, destino):
         self.matriz = matriz
         self.origen = origen
         self.destino = destino
         self.caminos = []
-
     def recursividad(self, actual, camino, distancia):
         if actual == self.destino:
             self.caminos.append((camino, distancia))
@@ -265,7 +257,6 @@ class BusquedaDeCamino:
         for s, d in enumerate(self.matriz[actual]):
             if d != 0 and s not in camino:
                 self.recursividad(s, camino + [s], distancia + d)
-
     def dfs(self):
         self.recursividad(self.origen, [self.origen], 0)
 
@@ -288,7 +279,6 @@ def usoMemoria():
 def menu_usuario(grafo):
     print()
     while True:
-        clear_output()
         print("--- Bienvenido usuario ---")
         print("1. Mostrar mapa")
         print("2. Calcular el camino más corto entre dos salas")
@@ -333,7 +323,7 @@ def menu_usuario(grafo):
         except ValueError as e:
             print(e)
 
-def main():
+def menu()
     grafo = Grafo()
     for i in range(12):
         grafo.grafo[str(i)] = ListaDobleEnlazada()
@@ -359,7 +349,6 @@ def main():
             if peso != 0:
                 grafo.grafo[str(i)].insertarFinal(str(j), peso)
     while True:
-        clear_output()
         print("--- Bienvenido ---")
         print("1. Entrar como Administrador")
         print("2. Entrar como Usuario")
@@ -393,5 +382,42 @@ def main():
                 raise ValueError("Opción no válida.")
         except ValueError as e:
             print(e)
+'''
+#           FIN DE LAS CLASES DE TERMINAL
+
+#           CLASES DE LOS MÉTODOS GRAFICOS
+
+class VentanaPrincipal:
+    def __init__(self):
+        self.ventanaPrincipal = tk.Tk()
+        self.ventanaPrincipal.title('RutaFacil')
+        self.ventanaPrincipal.geometry('800x600+600+100')
+        self.fondoDividido()
+
+        #-----------COSAS DE LA VENTANA PRINCIPAL----------
+
+        # Icono de la ventana
+        self.ventanaPrincipal.iconbitmap('Iconochicografo.ico')
+        # Texto de espacio
+        self.etiqueta_espacio1 = tk.Label(self.ventana, text="    ", font=("Arial", 12))
+        # Texto Titulo y Slogan de la aplicación
+        self.etiquetaTitulo = tk.Label(self.ventanaPrincipal, text="RutaFacil", font=("Century Gothic", 23))
+        self.etiquetaSlogan = tk.Label(self.ventanaPrincipal, text="¡Llega a tus destinos sin preocuparte!", font=("Century Gothic", 23))
+    def fondoDividido(self):
+        # Tamaños de la ventana
+        alto = 600
+        ancho = 800
+        # Color crema:
+        self.colorArriba = tk.Frame(self.ventanaPrincipal, bg='#fff6ef', width=ancho, height=alto//2)
+        self.colorArriba.pack(side='top', fill='x')
+        # Color naranja
+        self.colorAbajo = tk.Frame(self.ventanaPrincipal, bg='#fb8a49', width=ancho, height=alto//2)
+        self.colorAbajo.pack(side='bottom', fill='x')
+
+# 4.- ---------- Variables u objetos globales ----------
+# 5.- ---------- Bloque Principal ----------
 if __name__ == '__main__':
-    main()
+    aplicacionGrafo = VentanaPrincipal()
+    aplicacionGrafo.ventana.mainloop()
+    
+# 6.- ---------- Documentación y comentarios ----------
